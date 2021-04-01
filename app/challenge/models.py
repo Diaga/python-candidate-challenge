42fdb063311431a8813bcde2e26cd898cfb3ddd5
Diaga <diagaaos@gmail.com>
2021-04-01 10:12:28 +0500

    feat(django): setup django project

---
+++ app/challenge/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+from uuid import uuid4
+
+
+class GeoLocation(models.Model):
+    """Preset list storing location (long, lat)
+    with a human readable name"""
+
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    class Meta:
+        app_label = 'challenge'
+        default_related_name = 'geo_locations'
+
+    def __str__(self):
+        return f'{self.name} - ({self.latitude}, {self.longitude})'
+
+
+class FavoritesList(models.Model):
+    """Stores a search result list"""
+
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
